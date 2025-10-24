1874162b6a9a7fa36644adba05f4220fd19d9ee1
lisayaa07 <chanipron07112546@gmail.com>
2025-10-25 01:32:48 +0700

    ///

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
 require('dotenv').config();
 const express = require("express");
 const cors = require("cors");
-const connection = require("./db");
+
 const pool = require("./db"); // 👈 รับค่า pool เข้ามา
 const db = pool.promise();
 const app = express();
-app.use(cors());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
