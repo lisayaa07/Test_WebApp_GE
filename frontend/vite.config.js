ee8085433127f9df9e150a47b3829016a1838265
lisayaa07 <chanipron07112546@gmail.com>
2025-10-29 18:34:59 +0700

    /

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+import { defineConfig } from 'vite'       // ✅ ต้องมีบรรทัดนี้
+import vue from '@vitejs/plugin-vue'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vite.dev/config/
 export default defineConfig(({ mode }) => ({
-  plugins: [vue(), tailwindcss()],
+  plugins: [
+    vue(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
