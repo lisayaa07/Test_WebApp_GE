68bccaac7ace9df7a60a13bc48d2f7e5790e9ef6
lisayaa07 <chanipron07112546@gmail.com>
2025-10-30 01:14:35 +0700

    ลบโฟเดอร์

--- frontend/src/api/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios'
-
-const api = axios.create({
-  baseURL: '/api',           // ✅ ใช้ proxy แทน URL จริง
-  withCredentials: true,     // ✅ แนบ cookie
-  timeout: 15000,
-})
-
-export default api
