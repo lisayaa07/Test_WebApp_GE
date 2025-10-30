11890b03d0aeaf0bb5bbd3371560222234b067ac
lisayaa07 <chanipron07112546@gmail.com>
2025-10-30 23:13:02 +0700

    ลบโดเมน

--- backend/server.js
+++ backend/server.js
@@ -108,13 +108,13 @@ app.post('/login', async (req, res) => {
     const token = jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: '2h' });
 
    res.cookie('auth', token, {
-    httpOnly: true,
-    secure: true,
-    sameSite: 'none', // ✅ cross-domain ได้
-    domain: 'test-webapp-ge.onrender.com', // ✅ ต้องตรงกับ backend จริง
-    path: '/',
-    maxAge: 7 * 24 * 60 * 60 * 1000,
-  })
+      httpOnly: true,
+      secure: true,
+      sameSite: 'none',  // ✅ ต้องมี
+      path: '/',          // ✅ ต้องมี
+      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 วัน
+    })
+
 
 
 
